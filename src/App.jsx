464a93e84ac7e0fb3b3a7c23d212df9e00f77a91
Subramanyamb <whynotsubramanyam@gmail.com>
2023-03-24 21:20:22 +0530

    Few Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Intro from './components/intro/Intro';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
-import Topbar from './components/topbar/Topbar';
-
+import Home from './components/home/Home';
+import Walmart from './components/testimonials/Walmart';
+import JPMorgan from './components/testimonials/JPMorgan';
+import Att from './components/testimonials/Att';
+import IPrism from './components/testimonials/IPrism';
+import KTree from './components/testimonials/KTree'
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Topbar />
-      <Intro />
-      <Experience />
-      <Testimonials />
-      <Portfolio />
-      <Contact />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route exact path="/my-portfolio/" element={<Home />} />
+            <Route exact path="/my-portfolio/walmart" element={<Walmart />} />
+            <Route exact path="/my-portfolio/jpmorganchase" element={<JPMorgan />} />
+            <Route exact path="/my-portfolio/at-t" element={<Att />} />
+            <Route exact path="/my-portfolio/iprism" element={<IPrism />} />
+            <Route exact path="/my-portfolio/ktree" element={<KTree />} />
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Pagination } from 'swiper';
+import { Link } from 'react-router-dom';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -19,6 +20,7 @@ const Testimonials = () => {
       role: 'UI/UX Designer',
       timeline: 'Aug 2022 - Present',
       test: 'My Responsibilities included understanding the business requirements, analyze and implement the technical solution designing the web pages and the components using different UX tools InVision, Sketch, Zeplin, Adobe XD, and Figma. I was involved in User Research and Usability testing for both Web and Mobile Design for the digital applications. I developed highly scalable RESTful services utilizing MongoDB aggregation pipeline processing, and AngularJS 2.0 front-end to handle 30,000 user load for Confidential Readiness Dashboard.',
+      link: '/my-portfolio/walmart',
     },
     {
       id: 2,
@@ -27,6 +29,7 @@ const Testimonials = () => {
       role: 'UX Designer',
       timeline: 'Dec 2021 - July 2022',
       test: "I designed dynamic, cross-platform pages using HTML, CSS, JavaScript and with the addition of Bootstrap for a responsive menu navigation interface. I was responsible for designing, setting, communicating, and executing all user interface screens for each business requirement. I also designed highly usable user interface for applications using a combination of user centered design and agile development methods.",
+      link: "/my-portfolio/jpmorganchase",
     },
     {
       id: 3,
@@ -35,6 +38,7 @@ const Testimonials = () => {
       role: 'Web Designer',
       timeline: 'Jan 2021 - Nov 2021',
       test: 'I was involved in designing mock-ups for mobile web Application (iOS, Android) mostly using Sketch, Sketch and Adobe Creative Suite. I developed web presentation layer using HTML, CSS, JavaScript, ReactJS according to internal standards and guidelines. I used Axure to create wireframes for mobile-based applications before the development process.',
+      link: "/my-portfolio/at-t",
     },
     {
       id: 4,
@@ -43,6 +47,7 @@ const Testimonials = () => {
       role: 'UI Developer',
       timeline: 'Nov 2018 - Nov 2020',
       test: "I developed a web application by using Angular4 as front-end by using Web API to receive and pass data to back-end. I used Angular-CLI for initializing and for project setup with scaffolding features. I implemented Angular 4/2 component router for navigation, Angular 4/2 services to connect the web application to back-end APIs made use of ES6 features.",
+      link: "/my-portfolio/iprism",
     },
     {
       id: 5,
@@ -51,6 +56,7 @@ const Testimonials = () => {
       role: 'Junior Web Developer',
       timeline: 'Aug 2015 - Oct 2018',
       test: "I developed the User Interactive web pages in a professional manner using web technologies like HTML, XHTML and CSS based on the W3C and ADA standards. Developed client-side validation code using JavaScript and jQuery. Designed dynamic client-side JavaScript, codes to build web forms and simulate process for web application, page navigation and form validation. I also [articipated in Code review and Quality Assurance. Automated the functionality and interface testing of the application using Quick Test Professional (QTP).",
+      link: "/my-portfolio/ktree",
     },
   ];
   return (
@@ -70,6 +76,7 @@ const Testimonials = () => {
             <h4 className='client__name'>{test.role}</h4>
             <h6 className='client__name'>{test.timeline}</h6>
             <small className="client__review">{test.test}</small>
+            <button className='button_more'><Link to={test.link}>More Details</Link></button>
           </SwiperSlide>
         ))}
       </Swiper>
